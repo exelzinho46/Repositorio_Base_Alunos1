5be46fcf8633cd04b7051b4d32a471fd8a6a4046
hackerman <eliasreserva75@gmail.com>
2026-05-12 08:55:35 -0300

    Add files via upload

---
+++ Modulo 1/Extra/tabuadapersonalizada.py
@@ -0,0 +1,6 @@
+numero = int(input("digite o numero da tabuada: "))
+inicio = int(input("Digite onde a tabuada deve começa: "))
+fila = int(input("digite qual numero deve ir: "))
+
+for i in range(inicio fim + 1)
+print(f) 
